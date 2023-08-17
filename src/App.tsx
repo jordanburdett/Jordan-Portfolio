16fd7fad8901f646f4f33849ef32676b3d6cdf4a
Jordan Burdett <jordan@burdett.us>
2023-08-17 16:36:29 -0600

    Added dependencies.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <Button type="primary">Button TEST!</Button>
+    </>
+  )
+}
+
+export default App
