ee1a28bf3d1fa08f9a561d40e162c7b9e00455b8
Jordan Burdett <jordan@burdett.us>
2023-08-17 21:46:51 -0600

    added navbar with icons and future pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
-import './App.css'
-import { Button } from 'antd';
+import React from "react";
+import {
+  CodeOutlined,
+  DatabaseFilled,
+  GithubFilled,
+  HomeOutlined,
+  LaptopOutlined,
+  PhoneOutlined,
+  
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Button type="primary">Button TEST!</Button>
-    </>
-  )
+const { Header, Content, Sider } = Layout;
+
+const navItems: MenuProps["items"] = [
+  {
+    label: "Home",
+    key: "home",
+    icon: <HomeOutlined style={{color: "white"}}/>,
+  },
+  {
+    label: "Skills",
+    key: "skills",
+    icon: <DatabaseFilled />
+  },
+  { 
+    label: "Personal Projects", 
+    key: "personal projects",
+    icon: <CodeOutlined style={{color: "white"}}/>,
+  },
+  { 
+    label: "Work Experience", 
+    key: "work",
+    icon: <LaptopOutlined style={{color: "white"}}/>
+  },
+  { 
+    label: "Contact Me", 
+    key: "contact",
+    icon: <PhoneOutlined style={{color: "white"}}/>
+  },
+];
+
+const redirectToGithub = () => {
+  window.open("https://github.com/jordanburdett");
 }
 
-export default App
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <GithubFilled style={{color: "white", fontSize: "26px", marginRight: "15px"}} onClick={redirectToGithub}/>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["home"]}
+          items={navItems}
+        />
+      </Header>
+    </Layout>
+  );
+};
+
+export default App;
