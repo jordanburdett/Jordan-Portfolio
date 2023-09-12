37e39c7fc53d318dedfe4b8a739e97db11211626
Jordan Burdett <jordan@burdett.us>
2023-09-11 22:08:10 -0600

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,48 @@ import React from "react";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./Navigation/NavBar/NavBar";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Content/Home/Home";
+import About from "./Content/About/About";
+import Portfolio from "./Content/Portfolio/Portfolio";
+import Resume from "./Content/Resume/Resume";
+import Contact from "./Content/Contact/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 const App: React.FC = () => {
   return (
     <ChakraProvider>
       <NavBar />
+      <RouterProvider router={router} />
     </ChakraProvider>
   );
 };
--- src/Navigation/NavBar/NavBar.tsx
+++ src/Navigation/NavBar/NavBar.tsx
@@ -12,12 +12,14 @@ import { HiDocument } from "react-icons/hi";
 import { MdEmail } from "react-icons/md";
 
 
+
 const NavBar = () => {
+
   return (
     <HStack spacing="8" >
       <Box p="2" paddingLeft={"10"}>
         <Link href="/">
-          <HStack>
+          <HStack >
             <Icon as={AiFillHome} fontSize={"20px"}/>
             <Text fontSize={"xl"}>Home</Text>
           </HStack>
@@ -52,6 +54,14 @@ const NavBar = () => {
         <HStack>
             <Icon as={MdEmail} fontSize={"20px"}/>
             <Text fontSize={"xl"}>Contact</Text>
+          </HStack>
+        </Link>
+      </Box>
+      <Box p="2">
+        <Link href="/contact">
+        <HStack>
+            <Icon as={MdEmail} fontSize={"20px"}/>
+            <Text fontSize={"xl"}>Current path = {location.pathname}</Text>
           </HStack>
         </Link>
       </Box>
