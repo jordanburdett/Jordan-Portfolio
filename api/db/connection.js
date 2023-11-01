8747a446e400220220ba3090f3fc5dcb4be7db7a
Jordan Burdett <jordan@burdett.us>
2023-10-31 22:25:28 -0600

    fixed environment variable naming wrong

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const connectionString = process.env.MONGOCONNECTIONSTRING || "";
+const connectionString = process.env.MONGO_URI || "";
 const client = new MongoClient(connectionString);
 let db = client.db("Jordan-Portfolio");
 
