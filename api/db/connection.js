6b26695ff5b7ccd88c8c8f7e13e0c0a2fe2b6d27
Jordan Burdett <jordan@burdett.us>
2023-10-31 22:27:51 -0600

    fixed constant

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
-const connectionString = process.env.MONGO_URI || "";
+console.log("MONGODB_URI", process.env.MONGODB_URI);
+const connectionString = process.env.MONGODB_URI || "";
 const client = new MongoClient(connectionString);
 let db = client.db("Jordan-Portfolio");
 
